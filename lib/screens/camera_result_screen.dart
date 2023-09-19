83199dac28674600630518f236f1ca77bb4a52f4
dodo07070707 <aa01056328583@gmail.com>
2023-09-19 14:35:40 +0900

    image saving

--- lib/screens/camera_result_screen.dart
+++ lib/screens/camera_result_screen.dart
@@ -1,11 +1,35 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:jinlo_project/themes/color_theme.dart';
 import 'package:jinlo_project/themes/text_theme.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class PhotoPreview extends StatelessWidget {
   final String imagePath;
 
   const PhotoPreview({required this.imagePath, super.key});
+
+  Future<void> saveImage() async {
+    final nowDate = DateTime.now();
+    final nowFormattedDate = DateFormat('yyyy-MM-dd').format(nowDate);
+    // 이미지를 앱의 로컬 디렉토리에 저장하기 위한 경로 가져오기
+    final appDir = await getApplicationDocumentsDirectory();
+    final fileName = '$nowFormattedDate.jpg'; //!여기 수정
+    final localImage = File('${appDir.path}/$fileName');
+
+    // 이미지 복사
+    await File(imagePath).copy(localImage.path);
+
+    // 이미지 저장 완료 메시지 표시
+    Get.snackbar(
+      '알림',
+      '저장 완료.',
+      snackPosition: SnackPosition.BOTTOM,
+      duration: const Duration(seconds: 2),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,25 +83,77 @@ class PhotoPreview extends StatelessWidget {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    height: screenHeight / 844 * 64,
-                    width: screenWidth / 390 * 318,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(15.0),
+                  GestureDetector(
+                    child: Container(
+                      height: screenHeight / 844 * 64,
+                      width: screenWidth / 390 * 318,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.25),
+                            blurRadius: 4,
+                            spreadRadius: 0,
+                            offset: const Offset(4, 4),
+                          )
+                        ],
+                      ),
+                      child: const Center(
+                        child: Text(
+                          '다시 촬영하기',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Color(0xFFE81B00),
+                            fontSize: 18,
+                            fontFamily: 'Pretendard',
+                            fontWeight: FontWeight.w600,
+                            height: 1,
+                          ),
+                        ),
+                      ),
                     ),
-                    child: const Text(
-                      '다시 촬영하기',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                      color: Color(0xFFE81B00),
-                      fontSize: 18,
-                      fontFamily: 'Pretendard',
-                      fontWeight: FontWeight.w600,
-                      height: 1,
+                    onTap: () async {
+                      //Get.to()
+                    },
+                  ),
+                  SizedBox(height: screenHeight / 844 * 18),
+                  GestureDetector(
+                    child: Container(
+                      height: screenHeight / 844 * 64,
+                      width: screenWidth / 390 * 318,
+                      decoration: BoxDecoration(
+                          color: GRColors.MAIN_THEME,
+                          borderRadius: BorderRadius.circular(15.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.25),
+                              blurRadius: 4,
+                              spreadRadius: 0,
+                              offset: const Offset(4, 4),
+                            )
+                          ]),
+                      child: const Center(
+                        child: Text(
+                          '다음으로 !',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontFamily: 'Pretendard',
+                            fontWeight: FontWeight.w600,
+                            height: 1,
+                          ),
+                        ),
+                      ),
                     ),
-                    ),
-                  SizedBox(height: screenHeight / 844 * 128),
+                    onTap: () async {
+                      await saveImage();
+                      Get.snackbar('성공', 'ㅁㄴㅇㄹ',
+                          duration: const Duration(seconds: 2));
+                    },
+                  ),
+                  SizedBox(height: screenHeight / 844 * 110),
                 ],
               ),
             ],
